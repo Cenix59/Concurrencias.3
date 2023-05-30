f388014683739aba131e4c117cf4162ed199d1e6
Cenix59 <cenixgz@gmail.com>
2023-05-30 19:48:41 -0300

    Agregar archivos de proyecto.

---
+++ Concurrencias.3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Concurrencias._3
+{
+    public partial class Form1 : Form
+    {
+        private string apiURL;
+        private HttpClient httpClient;
+        public Form1()
+        {
+            InitializeComponent();
+            apiURL = "https://localhost:7284";
+            httpClient = new HttpClient();
+        }
+
+        private async void BtnCharge_Click(object sender, EventArgs e)
+        {
+            LoadingGIF.Visible = true;
+            await esperar();
+            var nombre = NameTxt.Text;
+            var saludo = await ObtenerSaludo(nombre);
+            MessageBox.Show(saludo);
+
+            LoadingGIF.Visible = false;
+        }
+
+        private async Task esperar()
+        {
+            await Task.Delay(5000);
+        }
+        private async Task<String> ObtenerSaludo(string nombre)
+        {
+            using (var respuesta = await httpClient.GetAsync($"{apiURL}/saludos/{nombre}"))
+            {
+                var saludo = await respuesta.Content.ReadAsStringAsync();
+                return saludo;
+            }
+        }
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
